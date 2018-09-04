1fb0779d48e9e6880ce799552d278fca61fa4cbf
yzhu <yzhu@anigo.com.au>
2018-09-04 15:48:33 +1000

    add doc to test

--- NameSorter.Test/NameSorterTest.cs
+++ NameSorter.Test/NameSorterTest.cs
@@ -7,6 +7,12 @@
     [TestFixture]
     public class NameSorterTest 
     {
+        /// <summary>
+        /// Test the basic relationship between INameSorter, INameSource, ISortAlgorithm
+        /// and INameDestination. In a sentence, INameSorter uses ISortAlgorithm to sort 
+        /// names provided by INameSource, the resulted name list is to be output to
+        /// INameDestination.
+        /// </summary>
         [Test]
         public void TestSort() {
            var mockSource = new Mock<INameSource>();
