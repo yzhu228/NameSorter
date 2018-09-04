c71938cd065e73d3e27a225e1aa692de45804b41
yzhu <yzhu@anigo.com.au>
2018-09-04 12:57:06 +1000

    initial commit

---
+++ NameSorter.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NameSorter.Test
+{
+    public class Class1
+    {
+    }
+}
---
+++ NameSorter/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using com.zhusmelb.Util.Logging;
+
+namespace NameSorter
+{
+    public class Class1
+    {
+        private static readonly ILogger _log 
+            = LogHelper.GetLogger(typeof(Class1).FullName);
+
+        public static void Main(string[] args) {
+            _log.Info("Main starts ...");
+            Console.WriteLine("Hellow World!");
+            _log.Info("Main ends.");
+        }
+    }
+}
