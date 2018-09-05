ada38c50b10bb876730ad28b6abcbf0a4fa9746e
yzhu <yzhu@anigo.com.au>
2018-09-05 17:11:49 +1000

    Add name-sorter app.

---
+++ NameSorter.App/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using NameSorter.Model;
+using NameSorter.Model.Implementation;
+
+namespace NameSorter.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName  = args[0];
+
+            using (var sorter = new SimpleNameSorter {
+                Source = new FileNameSource(fileName),
+                Algorithm = new LinqSortAlgorithm(),
+                Destinations = new List<INameDestination> {
+                    new ConsoleNameDestination(),
+                    new FileNameDestination(@"./sorted-names-list.txt")
+                }})
+            {
+                sorter.Sort();
+            }
+            
+        }
+    }
+}
