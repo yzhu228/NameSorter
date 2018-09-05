5a9a19e04d6c4a10835648d594df1b9de4a17202
yzhu <gzching@zhusmelb.com>
2018-09-05 22:30:16 +1000

    Update name-sort app

--- NameSorter.App/Program.cs
+++ NameSorter.App/Program.cs
@@ -1,27 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using NameSorter.Model;
-using NameSorter.Model.Implementation;
-
+﻿
 namespace NameSorter.App
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using CommandLine;
+    using NameSorter.Model;
+    using NameSorter.Model.Implementation;
+
+    class Options
+    {
+        [Value(0, MetaName="Input Filename", Required = true, HelpText = "Names file to be sorted.")]
+        public string InputFilename { get; set; }
+        [Option('o', "output", HelpText="Output file name")]
+        public string OutputFilename { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            string fileName  = args[0];
+            var result = Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(sortName);
+            return;
+        }
 
-            using (var sorter = new SimpleNameSorter {
-                Source = new FileNameSource(fileName),
-                Algorithm = new LinqSortAlgorithm(),
-                Destinations = new List<INameDestination> {
-                    new ConsoleNameDestination(),
-                    new FileNameDestination(@"./sorted-names-list.txt")
-                }})
-            {
-                sorter.Sort();
+        private static void sortName(Options option) {
+            var inFileName = option.InputFilename;
+            var outFileName = !string.IsNullOrWhiteSpace(option.OutputFilename) ?
+                option.OutputFilename : @"./sorted-names-list.txt";
+
+            try {
+                using (var sorter = new SimpleNameSorter {
+                    Source = new FileNameSource(inFileName),
+                    Algorithm = new LinqSortAlgorithm(),
+                    Destinations = new List<INameDestination> {
+                        new ConsoleNameDestination(),
+                        new FileNameDestination(outFileName)}
+                    })
+                {
+                    sorter.Sort();
+                }
             }
-            
+            catch (IOException ex) {
+                handleError(ex, "File operation error!");
+            }
+            catch (Exception ex) {
+                handleError(ex, "Unexpected error!");
+            }
+        }
+
+        private static void handleError(Exception ex, string message = "") {
+            Console.Error.WriteLine($"\nError: {message}");
+            Console.Error.WriteLine(ex.Message);
+            Console.Error.WriteLine();
         }
     }
 }
